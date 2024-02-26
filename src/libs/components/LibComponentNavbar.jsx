816a542c5dc3c6a7d3787bc7300a4053faed70dd
JovHariono <128022304+JovHariono@users.noreply.github.com>
2024-02-26 21:05:13 +0700

    navbar

--- src/libs/components/LibComponentNavbar.jsx
+++ src/libs/components/LibComponentNavbar.jsx
@@ -7,24 +7,25 @@ import useHTTP from "../hooks/useHTTP.jsx";
 const LibComponentNavbar = () => {
   const jwt = useJWT()
 
-  const applcation = useContext(ContextApplication);
+  const application = useContext(ContextApplication);
 
   const signOut = () => {
     jwt.signOut();
-    applcation.setIsAuthenticated(false);
+    application.setIsAuthenticated(false);
   }
 
   return (
     <Navbar expand="lg" className="bg-body-tertiary d-print-none">
       <Container>
-        <Navbar.Brand href="#">Loundry Management System</Navbar.Brand>
+        <Navbar.Brand href="#">Logo</Navbar.Brand>
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
-        {applcation.isAuthenticated && (
+        {application.isAuthenticated && (
           <Navbar.Collapse id="basic-navbar-nav">
             <Nav className="me-auto">
-              {/* <Nav.Link href="#/">Barang</Nav.Link>
-              <Nav.Link href="#terima">Terima</Nav.Link>
-              <Nav.Link href="#kas">Kas</Nav.Link> */}
+              <Nav.Link href="#/">Apotik</Nav.Link>              
+              <Nav.Link href="#terima">Pasien</Nav.Link>
+              <Nav.Link href="#kas">Kamar</Nav.Link>
+              <Nav.Link href="#kas">Staff</Nav.Link>
               <Nav.Link onClick={signOut}>Log Out</Nav.Link>
             </Nav>
           </Navbar.Collapse>
